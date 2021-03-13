cfc128ff3841d31178110d13c3b24ecc682057af
wengwentao-666 <2524308445@qq.com>
2021-03-13 17:15:46 +0800

    didu

---
+++ src/day05/animal.java
@@ -0,0 +1,11 @@
+package day05;
+
+public class animal {
+    String name;
+    public animal(String name){
+        this.name = name;
+    }
+    public void sleep(){
+        System.out.println("你猜");
+    }
+}
